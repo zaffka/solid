26212955ea46c5e3dee87f031c38410153c318c1
Aleksandr Zlobin <aleksandr.zlobin@quadcode.com>
2020-05-25 22:38:55 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+SOLID-principles illustrated by Go-code
---
+++ single/single.go
@@ -0,0 +1,58 @@
+package single
+
+import "database/sql"
+
+/*
+	INCORRECT VARIANT WITH "GOD OBJECT" (holds external dependencies and mixed logic)
+*/
+
+type BadEnvelope struct {
+	message string
+	db      *sql.DB
+}
+
+func (e *BadEnvelope) Pack(msg string) {
+	e.message = msg
+}
+
+func (e *BadEnvelope) Unpack() string {
+	return e.message
+}
+
+func (e *BadEnvelope) SaveToDB() error {
+
+	//SAVING ENVELOPE's MESSAGE TO DATABASE
+
+	return nil
+}
+
+/*
+	CORRECT VARIANT:
+	- Envelope's data being manipulated using struct's methods
+	- Database logic separated
+*/
+
+//Envelope type and it's methods
+type Envelope struct {
+	message string
+}
+
+func (e *Envelope) Pack(msg string) {
+	e.message = msg
+}
+
+func (e *Envelope) Unpack() string {
+	return e.message
+}
+
+//DB type, responsible for saving to the persistent storage
+type DB struct {
+	*sql.DB
+}
+
+func (d *DB) Save(env Envelope) error {
+
+	//SAVING ENVELOPE's MESSAGE TO DATABASE
+
+	return nil
+}
